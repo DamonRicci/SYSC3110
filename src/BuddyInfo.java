102988e4db86bf043d54cb5b104619fabe758d96
DamonRicci <damon.d.ricci@gmail.com>
2023-09-25 16:05:01 -0400

    First buddyinfo commit

---
+++ src/BuddyInfo.java
@@ -0,0 +1,12 @@
+public class BuddyInfo {
+
+    public BuddyInfo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+}
